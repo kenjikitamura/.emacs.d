b84b0cd19de464c54a33ee6459e3a71a9d1ed178
Kenji <kenji@rainbowdevil.jp>
2016-07-15 16:59:47 +0900

    Window位置保存

--- init.el
+++ init.el
@@ -841,4 +841,47 @@ static char * arrow_right[] = {
 (load "powerline-themes.el")
 (powerline-default-theme2)
 
+;; logcat-mode
+(require 'logcat)
 
+;; save windowsize
+(defconst my-save-frame-file
+  "~/.emacs.d/.framesize"
+  "フレームの位置、大きさを保存するファイルのパス")
+(defun my-save-frame-size()
+  "現在のフレームの位置、大きさを`my-save-frame-file'に保存します"
+  (interactive)
+  (let* ((param (frame-parameters (selected-frame)))
+         (current-height (frame-height))
+         (current-width (frame-width))
+         (current-top-margin (if (integerp (cdr (assoc 'top param)))
+                                 (cdr (assoc 'top param))
+                                 0))
+         (current-left-margin (if (integerp (cdr (assoc 'left param)))
+                                  (cdr (assoc 'left param))
+                                  0))
+         (buf nil)
+         (file my-save-frame-file)
+         )
+    ;; ファイルと関連付けられたバッファ作成
+    (unless (setq buf (get-file-buffer (expand-file-name file)))
+      (setq buf (find-file-noselect (expand-file-name file))))
+    (set-buffer buf)
+    (erase-buffer)
+    ;; ファイル読み込み時に直接評価させる内容を記述
+    (insert
+     (concat
+      "(set-frame-size (selected-frame) "(int-to-string current-width)" "(int-to-string current-height)")\n"
+      "(set-frame-position (selected-frame) "(int-to-string current-left-margin)" "(int-to-string current-top-margin)")\n"
+      ))
+    (save-buffer)))
+(defun my-load-frame-size()
+  "`my-save-frame-file'に保存されたフレームの位置、大きさを復元します"
+  (interactive)
+  (let ((file my-save-frame-file))
+    (when (file-exists-p file)
+        (load-file file))))
+
+(add-hook 'emacs-startup-hook 'my-load-frame-size)
+(add-hook 'kill-emacs-hook 'my-save-frame-size)
+(run-with-idle-timer 60 t 'my-save-frame-size)
