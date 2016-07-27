3e5b5daec8396b86a0cbc5602e9f694227137e60
Kenji <kenji@rainbowdevil.jp>
2016-07-27 10:17:00 +0900

    Add avy and migemo.

--- init.el
+++ init.el
@@ -41,6 +41,8 @@
     neotree
     git-gutter
     scala-mode2
+    avy
+    migemo
     yasnippet))
 (let ((not-installed
        (loop for package in my-package-list
@@ -891,3 +893,21 @@ static char * arrow_right[] = {
 
 ;; visible-bell
 (setq visible-bell t)
+
+;; migemo
+(require 'migemo)
+(setq migemo-command "cmigemo")
+(setq migemo-options '("-q" "--emacs"))
+
+;; Set your installed path
+(setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
+
+(setq migemo-user-dictionary nil)
+(setq migemo-regex-dictionary nil)
+(setq migemo-coding-system 'utf-8-unix)
+(load-library "migemo")
+(migemo-init)
+
+;; avy
+(global-set-key (kbd "C-;") 'avy-goto-char-timer)
+(global-set-key (kbd "C-j") 'avy-goto-line)
